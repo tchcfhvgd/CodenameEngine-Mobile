26cc14f83494e534c67a8462033ad966eab3a9ef
Chronic x3 <126361810+chronicsilly@users.noreply.github.com>
2025-06-16 11:49:48 +0100

    this has NOT been annoying me for ages (#649)

--- source/funkin/menus/ModSwitchMenu.hx
+++ source/funkin/menus/ModSwitchMenu.hx
@@ -11,8 +11,14 @@ class ModSwitchMenu extends MusicBeatSubstate {
 	var alphabets:FlxTypedGroup<Alphabet>;
 	var curSelected:Int = 0;
 
+	var subCam:FlxCamera;
+
 	public override function create() {
 		super.create();
+
+		camera = subCam = new FlxCamera();
+		subCam.bgColor = 0;
+		FlxG.cameras.add(subCam, false);
 
 		var bg = new FlxSprite(0, 0).makeSolid(FlxG.width, FlxG.height, 0xFF000000);
 		bg.updateHitbox();
@@ -62,6 +68,13 @@ class ModSwitchMenu extends MusicBeatSubstate {
 			alphabet.targetY = k - curSelected;
 		}
 		alphabets.members[curSelected].alpha = 1;
+	}
+
+	override function destroy() {
+		super.destroy();
+
+		if (FlxG.cameras.list.contains(subCam))
+			FlxG.cameras.remove(subCam);
 	}
 }
 #end
