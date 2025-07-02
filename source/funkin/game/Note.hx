b55fb41f404d3dabc1b404b15a9cccd818696134
Jade <45212377+crowplexus@users.noreply.github.com>
2025-07-02 09:00:36 -0400

    hey community can u check if left holds are still broken?

--- source/funkin/game/Note.hx
+++ source/funkin/game/Note.hx
@@ -154,9 +154,10 @@ class Note extends FlxSprite
 						case 0:
 							animation.addByPrefix('scroll', 'purple0');
 							animation.addByPrefix('hold', 'purple hold piece');
-							animation.addByPrefix('holdend', 'purple hold end');
 							if (frames.getByName("pruple end hold") != null)
 								animation.addByPrefix("holdend", "pruple end hold");
+							else
+								animation.addByPrefix('holdend', 'purple hold end');
 						case 1:
 							animation.addByPrefix('scroll', 'blue0');
 							animation.addByPrefix('hold', 'blue hold piece');
